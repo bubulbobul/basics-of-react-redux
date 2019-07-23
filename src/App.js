d9ccbfbedcae234e4a03d9d5fee31124d2ca97cf
windam <windam@live>
2019-07-23 19:16:04 +0530

    add post form post to the backend

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 
 import Posts from "./components/Posts";
+import PostForm from "./components/PostForm";
 
 function App() {
   return (
-    <div class='App'>
+    <div className='App'>
+      <PostForm />
+      <hr />
       <Posts />
+      <hr />
     </div>
   );
 }
