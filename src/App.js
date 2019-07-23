dfb72d5499e8c19a815402af887a0932516997d3
windam <windam@live>
2019-07-23 19:33:21 +0530

    add store

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./App.css";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 function App() {
   return (
-    <div className='App'>
-      <PostForm />
-      <hr />
-      <Posts />
-      <hr />
-    </div>
+    <Provider store={store}>
+      <div className='App'>
+        <PostForm />
+        <hr />
+        <Posts />
+        <hr />
+      </div>
+    </Provider>
   );
 }
 
