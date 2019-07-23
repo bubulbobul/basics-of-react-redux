1b23214a4a787341e41059baf8a78e57d66e71c9
windam <windam@live>
2019-07-23 20:13:45 +0530

    createpost from action

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
+import { connect } from "react-redux";
+import { createPost } from "../actions/postsAction";
 class PostForm extends Component {
   state = {
     title: "",
@@ -19,15 +22,8 @@ class PostForm extends Component {
       body: this.state.body
     };
 
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json"
-      },
-      body: JSON.stringify(post)
-    })
-      .then(res => res.json())
-      .then(data => console.log(data));
+    // call action
+    this.props.createPost(post);
   };
 
   render() {
@@ -62,4 +58,11 @@ class PostForm extends Component {
   }
 }
 
-export default PostForm;
+PostForm.propTypes = {
+  createPost: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { createPost }
+)(PostForm);
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -14,6 +14,11 @@ export default (state = initialState, action) => {
         ...state,
         items: action.payload
       };
+    case NEW_POST:
+      return {
+        ...state,
+        item: action.payload
+      };
     default:
       return state;
   }
