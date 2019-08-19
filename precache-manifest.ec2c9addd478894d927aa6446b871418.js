be0275eef09233cad86d63948ed72b4760992319
bubulbobul <windam@live.fr>
2019-08-19 11:23:38 +0530

    Updates

---
+++ precache-manifest.ec2c9addd478894d927aa6446b871418.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8edd4d86953a4ff6bccf2953f65f28f",
+    "url": "/basics-of-react-redux/index.html"
+  },
+  {
+    "revision": "dcb7c9f522d14ba569f7",
+    "url": "/basics-of-react-redux/static/css/main.2af469a7.chunk.css"
+  },
+  {
+    "revision": "7c5cde57d348400e9536",
+    "url": "/basics-of-react-redux/static/js/2.1d2f8073.chunk.js"
+  },
+  {
+    "revision": "dcb7c9f522d14ba569f7",
+    "url": "/basics-of-react-redux/static/js/main.2485a042.chunk.js"
+  },
+  {
+    "revision": "7d62da615479db40a14c",
+    "url": "/basics-of-react-redux/static/js/runtime~main.95f9d5e6.js"
+  }
+]);
